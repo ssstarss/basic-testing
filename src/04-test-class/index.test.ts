0c99b7c7d2557f6bb824b7632d83b14e53d5f692
ssstarss <s.s.star@mail.ru>
2024-10-21 20:46:01 +0700

    part of tests done

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,41 +1,70 @@
 // Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import { get } from 'lodash';
+import { BankAccount, getBankAccount } from '.';
 
 describe('BankAccount', () => {
   test('should create account with initial balance', () => {
-    // Write your test here
+    const testBankAccaunt = getBankAccount(1000);
+    expect(get(testBankAccaunt, '_balance')).toEqual(1000);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    const testBankAccaunt = getBankAccount(1000);
+    expect(() => testBankAccaunt.withdraw(1100)).toThrow(
+      `Insufficient funds: cannot withdraw more than ${1000}`,
+    );
   });
 
   test('should throw error when transferring more than balance', () => {
-    // Write your test here
+    const testBankAccaunt = getBankAccount(1000);
+    const newBankAccaunt = getBankAccount(500);
+    expect(() => testBankAccaunt.transfer(1100, newBankAccaunt)).toThrow(
+      `Insufficient funds: cannot withdraw more than ${1000}`,
+    );
   });
 
   test('should throw error when transferring to the same account', () => {
-    // Write your test here
+    const testBankAccaunt = getBankAccount(1000);
+    expect(() => testBankAccaunt.transfer(1100, testBankAccaunt)).toThrow(
+      'Transfer failed',
+    );
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    const testBankAccaunt = getBankAccount(1000);
+    testBankAccaunt.deposit(500);
+    expect(get(testBankAccaunt, '_balance')).toEqual(1500);
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    const testBankAccaunt = getBankAccount(1000);
+    testBankAccaunt.withdraw(500);
+    expect(get(testBankAccaunt, '_balance')).toEqual(500);
   });
 
   test('should transfer money', () => {
-    // Write your test here
+    const testBankAccaunt = getBankAccount(1000);
+    const newBankAccaunt = getBankAccount(1000);
+    testBankAccaunt.transfer(200, newBankAccaunt);
+    expect(get(testBankAccaunt, '_balance')).toEqual(800);
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
-    // Write your tests here
+    const testBankAccaunt = getBankAccount(1000);
+
+    try {
+      const fetchedBalance = await testBankAccaunt.fetchBalance();
+      expect(fetchedBalance).toBe(typeof Number);
+    } catch (e) {
+      expect(true).toBe(true);
+    }
   });
 
   test('should set new balance if fetchBalance returned number', async () => {
-    // Write your tests here
+    const testBankAccaunt = getBankAccount(1000);
+    testBankAccaunt.fetchBalance = jest.fn()
+    expect(()=>testBankAccaunt.fetchBalance())
+
   });
 
   test('should throw SynchronizationFailedError if fetchBalance returned null', async () => {
--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,8 +1,8 @@
 // Uncomment the code below and write your tests
-// import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
+ import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
 
 jest.mock('./index', () => {
-  // const originalModule = jest.requireActual<typeof import('./index')>('./index');
+   const originalModule = jest.requireActual<typeof import('./index')>('./index');
 });
 
 describe('partial mocking', () => {
